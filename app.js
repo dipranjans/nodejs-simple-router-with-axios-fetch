feb965506a1decd8c7ec04a02231e94097ebd975
Diptiranjan Sukla <sukla.dipti@gmail.com>
2020-03-08 16:02:43 +0530

    changes in app.js and routes/index.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const app = require("express")(); // express
 const routes = require("./routes"); // go to routes folder for routing
 const PORT = 3000; // port number
 
-// test is ok the routes
+// include the routes here
 app.use("/", routes);
 
 // listen to port 3000 serve
